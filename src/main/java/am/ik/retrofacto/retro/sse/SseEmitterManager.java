d18d46424ed838e8e6894d4a243cc4ffbc408c8a
Toshiaki Maki <makingx@gmail.com>
2024-02-20 09:58:36 +0900

    Monitor the number of subscribers

--- src/main/java/am/ik/retrofacto/retro/sse/SseEmitterManager.java
+++ src/main/java/am/ik/retrofacto/retro/sse/SseEmitterManager.java
@@ -8,6 +8,8 @@ import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 
 import am.ik.retrofacto.retro.event.CardEvent;
+import io.micrometer.core.instrument.FunctionCounter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,11 +23,23 @@ public class SseEmitterManager {
 
 	private final Logger log = LoggerFactory.getLogger(SseEmitterManager.class);
 
+	private final MeterRegistry meterRegistry;
+
+	public SseEmitterManager(MeterRegistry meterRegistry) {
+		this.meterRegistry = meterRegistry;
+	}
+
 	public SseEmitter newEmitter(String slug) {
 		SseEmitter emitter = new SseEmitter(TimeUnit.HOURS.toMillis(2));
 		UUID emitterId = UUID.randomUUID();
-		ConcurrentMap<UUID, SseEmitter> emitters = this.emittersMap.computeIfAbsent(slug,
-				k -> new ConcurrentHashMap<>());
+		ConcurrentMap<UUID, SseEmitter> emitters = this.emittersMap.computeIfAbsent(slug, k -> {
+			var map = new ConcurrentHashMap<UUID, SseEmitter>();
+			FunctionCounter.builder("retro_subscribers", map, ConcurrentHashMap::size)
+				.description("the number of retro subscribers")
+				.tag("slug", slug)
+				.register(this.meterRegistry);
+			return map;
+		});
 		emitters.put(emitterId, emitter);
 		emitter.onCompletion(() -> {
 			log.debug("Complete on emitter ({})", emitterId);
