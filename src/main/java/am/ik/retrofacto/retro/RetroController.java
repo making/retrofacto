17e7c51421e2b228ec974288b1973c3f81ccc7f1
Toshiaki Maki <makingx@gmail.com>
2024-02-20 09:46:07 +0900

    Introduce SseEmitterManager

--- src/main/java/am/ik/retrofacto/retro/RetroController.java
+++ src/main/java/am/ik/retrofacto/retro/RetroController.java
@@ -1,21 +1,12 @@
 package am.ik.retrofacto.retro;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.TimeUnit;
-
 import am.ik.retrofacto.retro.RetroService.UpdateCard;
 import am.ik.retrofacto.retro.event.CardCreateEvent;
 import am.ik.retrofacto.retro.event.CardDeleteEvent;
-import am.ik.retrofacto.retro.event.CardEvent;
 import am.ik.retrofacto.retro.event.CardLoadEvent;
 import am.ik.retrofacto.retro.event.CardUpdateEvent;
+import am.ik.retrofacto.retro.sse.SseEmitterManager;
 import io.hypersistence.tsid.TSID;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -40,14 +31,14 @@ public class RetroController {
 
 	private final CardRepository cardRepository;
 
-	private final ConcurrentMap<String, ConcurrentMap<UUID, SseEmitter>> emittersMap = new ConcurrentHashMap<>();
+	private final SseEmitterManager sseEmitterManager;
 
-	private final Logger log = LoggerFactory.getLogger(RetroController.class);
-
-	public RetroController(RetroService retroService, BoardRepository boardRepository, CardRepository cardRepository) {
+	public RetroController(RetroService retroService, BoardRepository boardRepository, CardRepository cardRepository,
+			SseEmitterManager sseEmitterManager) {
 		this.retroService = retroService;
 		this.boardRepository = boardRepository;
 		this.cardRepository = cardRepository;
+		this.sseEmitterManager = sseEmitterManager;
 	}
 
 	@PostMapping(path = "/boards")
@@ -63,44 +54,10 @@ public class RetroController {
 
 	@GetMapping(path = "/boards/{slug}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public SseEmitter cardEvents(@PathVariable String slug) {
-		SseEmitter emitter = new SseEmitter(TimeUnit.HOURS.toMillis(2));
-		UUID emitterId = UUID.randomUUID();
-		ConcurrentMap<UUID, SseEmitter> emitters = this.emittersMap.computeIfAbsent(slug,
-				k -> new ConcurrentHashMap<>());
-		emitters.put(emitterId, emitter);
-		emitter.onCompletion(() -> {
-			log.debug("Complete on emitter ({})", emitterId);
-			emitters.remove(emitterId);
-		});
-		emitter.onTimeout(() -> {
-			log.debug("Timeout on emitter ({})", emitterId);
-			emitters.remove(emitterId);
-		});
-		emitter.onError(e -> {
-			log.debug("Exception occurred on emitter (%s)".formatted(emitterId), e);
-			emitters.remove(emitterId);
-		});
-		this.boardRepository.findBySlug(slug).ifPresent(board -> {
-			try {
-				emitter.send(new CardLoadEvent(board));
-			}
-			catch (IOException e) {
-				log.info("Failed to send event on emitter (%s)".formatted(emitterId), e);
-			}
-		});
+		SseEmitter emitter = this.sseEmitterManager.newEmitter(slug);
+		this.boardRepository.findBySlug(slug)
+			.ifPresent(board -> this.sseEmitterManager.sendEvent(emitter, new CardLoadEvent(board)));
 		return emitter;
-	}
-
-	private void broadcastEvent(String slug, CardEvent cardEvent) {
-		ConcurrentMap<UUID, SseEmitter> emitters = this.emittersMap.get(slug);
-		for (Map.Entry<UUID, SseEmitter> entry : emitters.entrySet()) {
-			try {
-				entry.getValue().send(cardEvent);
-			}
-			catch (IOException e) {
-				log.debug("Failed to send event on emitter (%s)".formatted(entry.getKey()), e);
-			}
-		}
 	}
 
 	@PostMapping(path = "/boards/{slug}/cards")
@@ -113,7 +70,7 @@ public class RetroController {
 			.column(new Column(request.columnId()))
 			.build();
 		Card created = this.cardRepository.save(card);
-		this.broadcastEvent(slug, new CardCreateEvent(card));
+		this.sseEmitterManager.broadcastEvent(slug, new CardCreateEvent(card));
 		return ResponseEntity.created(builder.path("/cards/{cardId}").build(created.getId())).body(created);
 	}
 
@@ -121,20 +78,22 @@ public class RetroController {
 	public ResponseEntity<Void> updateCard(@PathVariable String slug, @PathVariable TSID cardId,
 			@RequestBody UpdateCard update) {
 		this.retroService.updateCard(cardId, update)
-			.ifPresent(card -> this.broadcastEvent(slug, new CardUpdateEvent(card)));
+			.ifPresent(card -> this.sseEmitterManager.broadcastEvent(slug, new CardUpdateEvent(card)));
 		return ResponseEntity.accepted().build();
 	}
 
 	@PostMapping(path = "/boards/{slug}/cards/{cardId}/like")
 	public ResponseEntity<Void> addLike(@PathVariable String slug, @PathVariable TSID cardId) {
-		this.retroService.addLike(cardId).ifPresent(card -> this.broadcastEvent(slug, new CardUpdateEvent(card)));
+		this.retroService.addLike(cardId)
+			.ifPresent(card -> this.sseEmitterManager.broadcastEvent(slug, new CardUpdateEvent(card)));
 		return ResponseEntity.accepted().build();
 	}
 
 	@DeleteMapping(path = "/boards/{slug}/cards/{cardId}")
 	public ResponseEntity<Void> deleteCard(@PathVariable String slug, @PathVariable TSID cardId) {
 		this.retroService.deleteCard(cardId)
-			.ifPresent(card -> this.broadcastEvent(slug, new CardDeleteEvent(cardId, card.getColumn().getId())));
+			.ifPresent(card -> this.sseEmitterManager.broadcastEvent(slug,
+					new CardDeleteEvent(cardId, card.getColumn().getId())));
 		return ResponseEntity.accepted().build();
 	}
 
---
+++ src/main/java/am/ik/retrofacto/retro/sse/SseEmitterManager.java
@@ -0,0 +1,68 @@
+package am.ik.retrofacto.retro.sse;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+
+import am.ik.retrofacto.retro.event.CardEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Component
+public class SseEmitterManager {
+
+	private final ConcurrentMap<String, ConcurrentMap<UUID, SseEmitter>> emittersMap = new ConcurrentHashMap<>();
+
+	private final Logger log = LoggerFactory.getLogger(SseEmitterManager.class);
+
+	public SseEmitter newEmitter(String slug) {
+		SseEmitter emitter = new SseEmitter(TimeUnit.HOURS.toMillis(2));
+		UUID emitterId = UUID.randomUUID();
+		ConcurrentMap<UUID, SseEmitter> emitters = this.emittersMap.computeIfAbsent(slug,
+				k -> new ConcurrentHashMap<>());
+		emitters.put(emitterId, emitter);
+		emitter.onCompletion(() -> {
+			log.debug("Complete on emitter ({})", emitterId);
+			emitters.remove(emitterId);
+		});
+		emitter.onTimeout(() -> {
+			log.debug("Timeout on emitter ({})", emitterId);
+			emitters.remove(emitterId);
+		});
+		emitter.onError(e -> {
+			log.debug("Exception occurred on emitter (%s)".formatted(emitterId), e);
+			emitters.remove(emitterId);
+		});
+		return emitter;
+	}
+
+	public void sendEvent(SseEmitter emitter, CardEvent cardEvent) {
+		try {
+			emitter.send(cardEvent);
+		}
+		catch (IOException e) {
+			log.info("Failed to send event on emitter", e);
+		}
+	}
+
+	public void broadcastEvent(String slug, CardEvent cardEvent) {
+		ConcurrentMap<UUID, SseEmitter> emitters = this.emittersMap.get(slug);
+		for (Map.Entry<UUID, SseEmitter> entry : emitters.entrySet()) {
+			try {
+				SseEmitter emitter = entry.getValue();
+				emitter.send(cardEvent);
+			}
+			catch (IOException e) {
+				UUID emitterId = entry.getKey();
+				log.debug("Failed to send event on emitter (%s)".formatted(emitterId), e);
+			}
+		}
+	}
+
+}
